10e1d4e4439dc7f21a6dc1f894247714a9979b27
Schildknecht Florent <florent.schildknecht@etu.utc.fr>
2015-07-19 11:46:26 +0200

    Remove GingerClient source code and set up dependency with composer

--- src/SDF/BilletterieBundle/Controller/billetController.php
+++ src/SDF/BilletterieBundle/Controller/billetController.php
@@ -2,6 +2,8 @@
 
 namespace SDF\BilletterieBundle\Controller;
 
+use Exception;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
--- src/SDF/BilletterieBundle/Controller/connexionController.php
+++ src/SDF/BilletterieBundle/Controller/connexionController.php
@@ -2,86 +2,92 @@
 
 namespace SDF\BilletterieBundle\Controller;
 
+use Exception;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Constraints\Date;
+
+use Ginger\Client\GingerClient;
+use Ginger\Client\ApiException;
+
 use SDF\BilletterieBundle\Entity\Utilisateur;
 use SDF\BilletterieBundle\Entity\UtilisateurExterieur;
 use SDF\BilletterieBundle\Entity\UtilisateurCAS;
 use SDF\BilletterieBundle\Entity\Log;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Constraints\Date;
 
 // xmlToArrayParser & CAS thx to github.com/robhub
-class xmlToArrayParser { 
-  /** The array created by the parser can be assigned to any variable: $anyVarArr = $domObj->array.*/ 
-  public  $array = array(); 
-  public  $parse_error = false; 
-  private $parser; 
-  private $pointer; 
-  
-  /** Constructor: $domObj = new xmlToArrayParser($xml); */ 
-  public function __construct($xml) { 
-    $this->pointer =& $this->array; 
-    $this->parser = xml_parser_create("UTF-8"); 
-    xml_set_object($this->parser, $this); 
-    xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false); 
-    xml_set_element_handler($this->parser, "tag_open", "tag_close"); 
-    xml_set_character_data_handler($this->parser, "cdata"); 
-    $this->parse_error = xml_parse($this->parser, ltrim($xml))? false : true; 
-  } 
-  
-  /** Free the parser. */ 
-  public function __destruct() { xml_parser_free($this->parser);} 
-  /** Get the xml error if an an error in the xml file occured during parsing. */ 
-  public function get_xml_error() { 
-    if($this->parse_error) { 
-      $errCode = xml_get_error_code ($this->parser); 
-      $thisError =  "Error Code [". $errCode ."] \"<strong style='color:red;'>" . xml_error_string($errCode)."</strong>\", 
-                            at char ".xml_get_current_column_number($this->parser) . " 
-                            on line ".xml_get_current_line_number($this->parser).""; 
-    }else $thisError = $this->parse_error; 
-    return $thisError; 
-  } 
-  
-  private function tag_open($parser, $tag, $attributes) { 
-    $this->convert_to_array($tag, 'attrib'); 
-    $idx=$this->convert_to_array($tag, 'cdata'); 
-    if(isset($idx)) { 
-      $this->pointer[$tag][$idx] = Array('@idx' => $idx,'@parent' => &$this->pointer); 
-      $this->pointer =& $this->pointer[$tag][$idx]; 
-    }else { 
-      $this->pointer[$tag] = Array('@parent' => &$this->pointer); 
-      $this->pointer =& $this->pointer[$tag]; 
-    } 
-    if (!empty($attributes)) { $this->pointer['attrib'] = $attributes; } 
-  } 
-  /** Adds the current elements content to the current pointer[cdata] array. */ 
-  private function cdata($parser, $cdata) { $this->pointer['cdata'] = trim($cdata); } 
-  private function tag_close($parser, $tag) { 
-    $current = & $this->pointer; 
-    if(isset($this->pointer['@idx'])) {unset($current['@idx']);} 
-    
-    $this->pointer = & $this->pointer['@parent']; 
-    unset($current['@parent']); 
-    
-    if(isset($current['cdata']) && count($current) == 1) { $current = $current['cdata'];} 
-    else if(empty($current['cdata'])) {unset($current['cdata']);} 
-  } 
-  
-  /** Converts a single element item into array(element[0]) if a second element of the same name is encountered. */ 
-  private function convert_to_array($tag, $item) { 
-    if(isset($this->pointer[$tag][$item])) { 
-      $content = $this->pointer[$tag]; 
-      $this->pointer[$tag] = array((0) => $content); 
-      $idx = 1; 
-    }else if (isset($this->pointer[$tag])) { 
-      $idx = count($this->pointer[$tag]); 
-      if(!isset($this->pointer[$tag][0])) { 
-        foreach ($this->pointer[$tag] as $key => $value) { 
-            unset($this->pointer[$tag][$key]); 
-            $this->pointer[$tag][0][$key] = $value; 
-    }}}else $idx = null; 
-    return $idx; 
-  } 
+class xmlToArrayParser {
+  /** The array created by the parser can be assigned to any variable: $anyVarArr = $domObj->array.*/
+  public  $array = array();
+  public  $parse_error = false;
+  private $parser;
+  private $pointer;
+
+  /** Constructor: $domObj = new xmlToArrayParser($xml); */
+  public function __construct($xml) {
+    $this->pointer =& $this->array;
+    $this->parser = xml_parser_create("UTF-8");
+    xml_set_object($this->parser, $this);
+    xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
+    xml_set_element_handler($this->parser, "tag_open", "tag_close");
+    xml_set_character_data_handler($this->parser, "cdata");
+    $this->parse_error = xml_parse($this->parser, ltrim($xml))? false : true;
+  }
+
+  /** Free the parser. */
+  public function __destruct() { xml_parser_free($this->parser);}
+  /** Get the xml error if an an error in the xml file occured during parsing. */
+  public function get_xml_error() {
+    if($this->parse_error) {
+      $errCode = xml_get_error_code ($this->parser);
+      $thisError =  "Error Code [". $errCode ."] \"<strong style='color:red;'>" . xml_error_string($errCode)."</strong>\",
+                            at char ".xml_get_current_column_number($this->parser) . "
+                            on line ".xml_get_current_line_number($this->parser)."";
+    }else $thisError = $this->parse_error;
+    return $thisError;
+  }
+
+  private function tag_open($parser, $tag, $attributes) {
+    $this->convert_to_array($tag, 'attrib');
+    $idx=$this->convert_to_array($tag, 'cdata');
+    if(isset($idx)) {
+      $this->pointer[$tag][$idx] = Array('@idx' => $idx,'@parent' => &$this->pointer);
+      $this->pointer =& $this->pointer[$tag][$idx];
+    }else {
+      $this->pointer[$tag] = Array('@parent' => &$this->pointer);
+      $this->pointer =& $this->pointer[$tag];
+    }
+    if (!empty($attributes)) { $this->pointer['attrib'] = $attributes; }
+  }
+  /** Adds the current elements content to the current pointer[cdata] array. */
+  private function cdata($parser, $cdata) { $this->pointer['cdata'] = trim($cdata); }
+  private function tag_close($parser, $tag) {
+    $current = & $this->pointer;
+    if(isset($this->pointer['@idx'])) {unset($current['@idx']);}
+
+    $this->pointer = & $this->pointer['@parent'];
+    unset($current['@parent']);
+
+    if(isset($current['cdata']) && count($current) == 1) { $current = $current['cdata'];}
+    else if(empty($current['cdata'])) {unset($current['cdata']);}
+  }
+
+  /** Converts a single element item into array(element[0]) if a second element of the same name is encountered. */
+  private function convert_to_array($tag, $item) {
+    if(isset($this->pointer[$tag][$item])) {
+      $content = $this->pointer[$tag];
+      $this->pointer[$tag] = array((0) => $content);
+      $idx = 1;
+    }else if (isset($this->pointer[$tag])) {
+      $idx = count($this->pointer[$tag]);
+      if(!isset($this->pointer[$tag][0])) {
+        foreach ($this->pointer[$tag] as $key => $value) {
+            unset($this->pointer[$tag][$key]);
+            $this->pointer[$tag][0][$key] = $value;
+    }}}else $idx = null;
+    return $idx;
+  }
 }
 
 class CAS
@@ -99,191 +105,12 @@ class CAS
 	public static function login()
 	{
 		header('Location: '.self::URL.'login?service=http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-	} 
+	}
 	public static function logout()
 	{
 		header('Location: '.self::URL.'logout?service=http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));//ou SCRIPT_NAME?
 		// On n'utilise pas REQUEST_URI sinon cela déconnecterait à l'infini.
 	}
-}
-
-class KoalaClient {
-  protected $url = "";
-  protected $useragent = "KoalaClient/0.1";
-
-  protected function apiCall($endpoint, $params = array(), $method = "GET") {
-    // Construction de la chaîne de paramètres
-    $paramstring = "";
-    if (!empty($params)) {
-      foreach ($params as $key => $param) {
-        $paramstring .= $key . "=" . $param . "&";
-      }
-      // On supprimer le dernier &
-      $paramstring = substr($paramstring, 0, -1);
-    } // $paramstring = "key=[clé]&"
-    
-    // Réglages de cURL
-    $settings = array(
-      CURLOPT_USERAGENT => $this->useragent,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_CUSTOMREQUEST => $method,
-      CURLOPT_SSL_VERIFYPEER => true,
-      CURLOPT_CAINFO => __DIR__. '/../Resources/certificates/TERENA_SSL_CA.pem',
-    );
-    
-    // Construction de l'URL et des postfields
-    if($method == "GET"){
-      $url = $this->url . $endpoint . "?" . $paramstring;
-    }
-    else {
-      $url = $this->url . $endpoint;
-      $settings[CURLOPT_POSTFIELDS] = $params;
-    }
-    
-    // Initialisation de cURL
-    $ch = curl_init($url);
-    curl_setopt_array($ch, $settings);
-
-    // Éxécution de la requête
-    $result = curl_exec($ch);
-    
-    // Si erreur d'appel de cron
-    if (curl_errno($ch) != 0) {
-      throw new ApiException(503);
-    }
-    // Si erreur http, on la renvoie
-    else if (curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200) {
-      throw new ApiException(curl_getinfo($ch, CURLINFO_HTTP_CODE));
-    }
-    // Sinon, on renvoie les infos
-    else {
-      return json_decode($result);
-    }
-  }
-}
-
-class GingerClient extends KoalaClient {
-  private $key;
-
-  public function __construct($key, $url="https://assos.utc.fr/ginger/v1/"){
-    $this->url = $url;
-    $this->key = $key;
-  }
-  
-  public function apiCall($endpoint, $params = array(), $method = "GET") {
-    // Ajout de la clé aux requêtes et appel du parent
-    $params["key"] = $this->key;
-    return parent::apiCall($endpoint, $params, $method);
-  }
-  
-  /**
-   * Récupérer un utilisateur à partir d'un login.
-   *
-   * @param string $login Login
-   * @return object Utilisateur
-   */
-  public function getUser($login) {
-    return $this->apiCall($login);
-  }
-  
-  /**
-   * Récupérer un utilisateur à partir d'un id de badge
-   * (si la clé l'autorise).
-   *
-   * @param string $badge Identifiant de badge
-   * @return object Utilisateur
-   */
-  public function getCard($badge) {
-    return $this->apiCall("badge/$badge");
-  }
-  
-  public function findPersonne($loginPart) {
-    return $this->apiCall("find/$loginPart");
-  }
-  
-  public function getCotisations($login) {
-    return $this->apiCall("$login/cotisations");
-  }
-  
-  public function addCotisation($login, $debut, $fin, $montant){
-    $params = array(
-      "debut" => $debut,
-      "fin" => $fin,
-      "montant" => $montant,
-    );
-    return $this->apiCall("$login/cotisations", $params, "POST");
-  }
-  
-  /**
-   * Récupérer l'historique des cotisations par semestre
-   * (nécessite une clé avec le droit cotisation).
-   *
-   * @return array Stats (semestre => nombre de cotisations)
-   */
-  public function getStats() {
-    return $this->apiCall("stats");
-  }
-
- public function getExts() {
-    return $this->apiCall("exts");
-  }
-  
-  public function setPersonne($login, $nom, $prenom, $mail, $is_adulte){
-    $params = array(
-      "nom" => $nom,
-      "prenom" => $prenom,
-      "is_adulte" => $is_adulte,
-    );
-    return $this->apiCall("$login/edit", $params, "POST");
-  }
-}
-
-class ApiException extends \Exception {
-  static $http = array (
-    100 => "Continue",
-    101 => "Switching Protocols",
-    200 => "OK",
-    201 => "Created",
-    202 => "Accepted",
-    203 => "Non-Authoritative Information",
-    204 => "No Content",
-    205 => "Reset Content",
-    206 => "Partial Content",
-    300 => "Multiple Choices",
-    301 => "Moved Permanently",
-    302 => "Found",
-    303 => "See Other",
-    304 => "Not Modified",
-    305 => "Use Proxy",
-    307 => "Temporary Redirect",
-    400 => "Bad Request",
-    401 => "Non autorisé : une clé d'accès est nécessaire pour exécuter cette requête",
-    402 => "Payment Required",
-    403 => "Interdit : l'authentification est refusée",
-    404 => "Non trouvé : la ressource demandée n'existe pas",
-    405 => "Method Not Allowed",
-    406 => "Not Acceptable",
-    407 => "Proxy Authentication Required",
-    408 => "Request Time-out",
-    409 => "Conflict",
-    410 => "Gone",
-    411 => "Length Required",
-    412 => "Precondition Failed",
-    413 => "Request Entity Too Large",
-    414 => "Request-URI Too Large",
-    415 => "Unsupported Media Type",
-    416 => "Requested range not satisfiable",
-    417 => "Expectation Failed",
-    500 => "Internal Server Error",
-    501 => "Not Implemented",
-    502 => "Bad Gateway",
-    503 => "Service Unavailable",
-    504 => "Gateway Time-out"        
-  );
-  public function __construct($code) {
-    parent::__construct($this::$http[$code], $code);
-    $this->code = $code;
-  }
 }
 
 function checkIfUserInDatabase($userRecupere){
@@ -323,7 +150,7 @@ class connexionController extends Controller
 
       if(!isset($_GET['ticket'])) return $this->redirect('https://cas.utc.fr/cas/login?service=http://'.$_SERVER["HTTP_HOST"].$this->get('router')->generate('sdf_billetterie_connexionCAS'));
         // pas de ticket reçu : pas de connexion CAS initialisée : on redirige vers la page CAS
-    	   
+
         // SINON : ticket reçu : connexion initialisée, il faut récupérer le token
       try {
         $retourCAS = $this->recuperationLogin();
@@ -453,7 +280,7 @@ class connexionController extends Controller
 
       $extUser = new UtilisateurExterieur();
       $user = new Utilisateur();
-      
+
       $user->setNom($_POST['nom']);
       $user->setPrenom($_POST['prenom']);
       $user->setEmail($_POST['mail']);
